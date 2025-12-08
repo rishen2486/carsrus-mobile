c2f0d28ecd9696790d7f300260546828aafef18e
rishen2486 <rishenarmoogum@yahoo.com>
2025-12-08 11:16:29 +0400

    Remove StrictMode wrapper from render method

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
@@ -6,11 +5,9 @@ import "./index.css";
 import { CurrencyProvider } from "./contexts/CurrencyContext";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <CurrencyProvider>
-        <App />
-      </CurrencyProvider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <CurrencyProvider>
+      <App />
+    </CurrencyProvider>
+  </BrowserRouter>
 );
